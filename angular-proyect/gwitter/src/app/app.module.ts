61d006e2b929a9c5671e1d5899133cfa2bfc7422
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2020-03-27 20:31:25 -0300

    Test de crear los compoentes
    Solo un test, nada mas

--- angular-proyect/gwitter/src/app/app.module.ts
+++ angular-proyect/gwitter/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule
