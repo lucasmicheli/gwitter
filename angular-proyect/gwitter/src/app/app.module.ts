680ded845a4bb418efd3451d0e58824886962c7a
Lucas Micheli <lucasmicheli@outlook.com>
2020-04-02 01:29:54 -0300

    Changes to app.module, search & trends components

--- angular-proyect/gwitter/src/app/app.module.ts
+++ angular-proyect/gwitter/src/app/app.module.ts
@@ -13,10 +13,16 @@ import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { TweetDetailsComponent } from './tweet-details/tweet-details.component';
 import { ScreenUserPipe } from './pipe/screen-user.pipe';
+import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TrendsComponent } from './trends/trends.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, TweetDetailsComponent, ScreenUserPipe],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, TweetDetailsComponent, ScreenUserPipe, SearchComponent, TrendsComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, InfiniteScrollModule, NgxSpinnerModule, FontAwesomeModule],
   providers: [TwitterAPIService],
   bootstrap: [AppComponent]
 })
--- angular-proyect/gwitter/src/app/service/twitter-api.service.ts
+++ angular-proyect/gwitter/src/app/service/twitter-api.service.ts
@@ -40,4 +40,8 @@ export class TwitterAPIService {
       })
     );
   }
+
+  getSearchTweets (searchQuery: string): Observable<Tweet> {
+    return this.http.get<Tweet>(this.urlSearchTweets+searchQuery)
+  };
 }
